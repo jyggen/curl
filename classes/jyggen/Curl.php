843bb87e7fe25d71cf9c9dff29d816704308c100
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-12-19 00:56:21 +0100

    Moved helper logic into Curl::makeRequest().

--- classes/jyggen/Curl.php
+++ classes/jyggen/Curl.php
@@ -24,7 +24,26 @@ class Curl
 	 * @param	mixed	$url
 	 * @return	array
 	 */
-	public static function delete($urls);
+	public static function delete($url)
+	{
+
+		if (!is_array($url)) {
+
+			$urls = array($url => null);
+
+		} else {
+
+			foreach ($url as $value) {
+
+				$urls[$value] = null;
+
+			}
+
+		}
+
+		return static::makeRequest('DELETE', $urls);
+
+	}
 
 	/**
 	 * Static helper to do GET requests.
@@ -32,43 +51,24 @@ class Curl
 	 * @param	mixed	$url
 	 * @return	array
 	 */
-	public static function get($urls)
+	public static function get($url)
 	{
 
-		// Create a new Dispatcher.
-		$dispatcher = new Dispatcher;
+		if (!is_array($url)) {
 
-		// Turn $urls into an array if it isn't.
-		if (!is_array($urls)) { $urls = array($urls); }
+			$urls = array($url => null);
 
-		// Foreach $urls:
-		foreach ($urls as $url) {
-
-			// Create a new Session.
-			$session = new Session($url);
-
-			// Follow any 3xx HTTP status code.
-			$session->setOption(CURLOPT_FOLLOWLOCATION, true);
-
-			// Add the session to the dispatcher.
-			$dispatcher->addSession($session);
-
-		}
-
-		// Execute the request(s).
-		$responses = $dispatcher->execute()->getResponses();
-
-		// If more than one URL was requested:
-		if(count($urls) > 1) {
-
-			return $responses;
-
-		// Else:
 		} else {
 
-			return $responses[0];
+			foreach ($url as $value) {
+
+				$urls[$value] = null;
+
+			}
 
 		}
+
+		return static::makeRequest('GET', $urls);
 
 	}
 
@@ -79,17 +79,61 @@ class Curl
 	 * @param	array	$data
 	 * @return	array
 	 */
-	public static function post($urls, $data = null)
+	public static function post($urls, array $data = null)
+	{
+
+		if (!is_array($urls)) {
+
+			$urls = array($urls => $data);
+
+		}
+
+		return static::makeRequest('POST', $urls);
+
+	}
+
+	/**
+	 * Static helper to do PUT requests.
+	 *
+	 * @param	mixed	$urls
+	 * @param	array	$data
+	 * @return	array
+	 */
+	public static function put($urls, array $data = null)
+	{
+
+		if (!is_array($urls)) {
+
+			$urls = array($urls => $data);
+
+		}
+
+		return static::makeRequest('PUT', $urls);
+
+	}
+
+	/**
+	 * Setup and execute a HTTP request.
+	 *
+	 * @param	string	$method
+	 * @param	array	$urls
+	 * @return	array
+	 */
+	protected static function makeRequest($method, $urls)
 	{
 
 		// Create a new Dispatcher.
 		$dispatcher = new Dispatcher;
 
-		// Turn $urls into an array if it isn't.
-		if (!is_array($urls)) { $urls = array($urls => $data); }
-
 		// Foreach $urls:
 		foreach ($urls as $url => $data) {
+
+			if($data !== null) {
+
+				$data = http_build_query($data);
+
+			}
+
 
 			// Create a new Session.
 			$session = new Session($url);
@@ -97,9 +141,35 @@ class Curl
 			// Follow any 3xx HTTP status code.
 			$session->setOption(CURLOPT_FOLLOWLOCATION, true);
 
-			// Add the POST data to the session.
-			$session->setOption(CURLOPT_POST, true);
-			$session->setOption(CURLOPT_POSTFIELDS, $data);
+			if ($method === 'DELETE') {
+
+				// Set request method to DELETE.
+				$session->setOption(CURLOPT_CUSTOMREQUEST, 'DELETE');
+
+			} elseif ($method === 'POST') {
+
+				// Add the POST data to the session.
+				$session->setOption(CURLOPT_POST, true);
+				$session->setOption(CURLOPT_POSTFIELDS, $data);
+
+			} elseif ($method === 'PUT') {
+
+				// Write the PUT data to memory.
+				$fh = fopen('php://memory', 'rw');
+				fwrite($fh, $data);
+				rewind($fh);
+
+				// Add the PUT data to the session.
+				$session->setOption(CURLOPT_INFILE, $fh);
+				$session->setOption(CURLOPT_INFILESIZE, mb_strlen($data, 'UTF-8'));
+				$session->setOption(CURLOPT_PUT, true);
+
+			} else {
+
+				// Redundant, but reset the method to GET.
+				$session->setOption(CURLOPT_HTTPGET, true);
+
+			}
 
 			// Add the session to the dispatcher.
 			$dispatcher->addSession($session);
@@ -122,19 +192,5 @@ class Curl
 		}
 
 	}
-
-	/**
-	 * Static helper to do PUT requests.
-	 *
-	 * @param	mixed	$url
-	 * @return	array
-	 */
-	public static function put($urls, $data = null);
-
-	/*
-	 * @todo refactor Curl::get() and Curl::post() to utilize
-	 *       this method to avoid code repetition.
-	 */
-	protected static function makeRequest($method, $urls);
 
 }
