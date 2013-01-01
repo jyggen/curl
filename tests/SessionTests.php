967550be3ab02af03d9e4f9464bf199c1799dfda
Jonas Stendahl <jonas.stendahl@gmail.com>
2013-01-01 18:56:25 +0100

    Lots of unit testing for Session.

--- tests/SessionTests.php
+++ tests/SessionTests.php
@@ -15,18 +15,18 @@ use jyggen\Curl\Session;
 class SessionTests extends PHPUnit_Framework_TestCase
 {
 
-	protected $instance;
+	protected static $instance;
 
-	public function getInstance()
+	public static function getInstance()
 	{
 
-		if($this->instance == null) {
+		if(static::$instance == null) {
 
-			$this->instance = new Session('http://example.com/');
+			static::$instance = new Session('http://example.com/');
 
 		}
 
-		return $this->instance;
+		return static::$instance;
 
 	}
 
@@ -40,9 +40,95 @@ class SessionTests extends PHPUnit_Framework_TestCase
 	public function testGetHandle()
 	{
 
-		$session = $this->getInstance();
-		$this->assertInternalType('resource', $session->getHandle());
-		$this->assertEquals('curl', get_resource_type($session->getHandle()));
+		$handle = static::getInstance()->getHandle();
+
+		$this->assertInternalType('resource', $handle);
+		$this->assertEquals('curl', get_resource_type($handle));
+
+	}
+
+	public function testGetInfo()
+	{
+
+		$info = static::getInstance()->getInfo();
+
+		$this->assertEquals('http://example.com/', $info['url']);
+
+	}
+
+	public function testGetInfoWithKey()
+	{
+
+		$this->assertEquals('http://example.com/', static::getInstance()->getInfo(CURLINFO_EFFECTIVE_URL));
+
+	}
+
+	public function testGetResponseBeforeExecute()
+	{
+
+		$this->assertNull(static::getInstance()->getResponse());
+
+	}
+
+	public function testSetOption()
+	{
+
+		$session = static::getInstance();
+
+		$this->assertTrue($session->setOption(CURLOPT_URL, 'http://example.org/'));
+		$this->assertEquals('http://example.org/', $session->getInfo(CURLINFO_EFFECTIVE_URL));
+
+	}
+
+	public function testSetOptionArray()
+	{
+
+		$session = static::getInstance();
+
+		$this->assertTrue($session->setOption(array(CURLOPT_FOLLOWLOCATION => true, CURLOPT_URL => 'http://example.com/')));
+		$this->assertEquals('http://example.com/', $session->getInfo(CURLINFO_EFFECTIVE_URL));
+
+	}
+
+	public function testSetProtectedOption()
+	{
+
+		$this->setExpectedException('jyggen\\ProtectedOptionException');
+		static::getInstance()->setOption(CURLOPT_RETURNTRANSFER, true);
+
+	}
+
+	public function testSetResponseWithBool()
+	{
+
+		$session = static::getInstance();
+
+		$session->setResponse(true);
+
+		$response = $session->getResponse();
+
+		$this->assertFalse(array_key_exists('data', $response));
+		$this->assertTrue(array_key_exists('info', $response));
+		$this->assertEquals(0, $response['info']['http_code']);
+		$this->assertEquals('http://example.com/', $response['info']['url']);
+
+	}
+
+	public function testSetResponseWithContent()
+	{
+
+		$session = static::getInstance();
+		$data    = curl_exec($session->getHandle());
+
+		$session->setResponse($data);
+
+		$response = $session->getResponse();
+
+		$this->assertTrue(array_key_exists('data', $response));
+		$this->assertTrue(array_key_exists('info', $response));
+		$this->assertEquals(200, $response['info']['http_code']);
+		$this->assertEquals('http://www.iana.org/domains/example/', $response['info']['url']);
+		$this->assertSelectEquals('html body div h1', 'Example Domain', true, $response['data']);
 
 	}
 
