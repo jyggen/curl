5fc270a04512b75f4d42e43743c90ddbf80888dd
Jonas Stendahl <jonas.stendahl@gmail.com>
2013-01-07 13:26:37 +0100

    Added tests for Dispatcher::addSession().

--- tests/DispatcherTests.php
+++ tests/DispatcherTests.php
@@ -11,6 +11,7 @@
  */
 
 use jyggen\Curl\Dispatcher;
+use jyggen\Curl\Session;
 
 class DispatcherTests extends PHPUnit_Framework_TestCase
 {
@@ -20,7 +21,7 @@ class DispatcherTests extends PHPUnit_Framework_TestCase
 	public static function getInstance()
 	{
 
-		if(static::$instance == null) {
+		if (static::$instance == null) {
 
 			static::$instance = new Dispatcher;
 
@@ -36,6 +37,51 @@ class DispatcherTests extends PHPUnit_Framework_TestCase
 		$dispatcher = static::getInstance();
 
 		$this->assertInstanceOf('jyggen\\Curl\\Dispatcher', $dispatcher);
+
+	}
+
+	public function testGetSessions()
+	{
+
+		$sessions = static::getInstance()->getSessions();
+
+		$this->assertTrue(is_array($sessions));
+		$this->assertTrue(empty($sessions));
+
+	}
+
+	public function testAddSessionSingle()
+	{
+
+		$dispatcher = static::getInstance();
+		$session    = new Session('http://example.com/');
+
+		$dispatcher->addSession($session);
+
+		$sessions = $dispatcher->getSessions();
+		$this->assertEquals('http://example.com/', $sessions[0]->getInfo(CURLINFO_EFFECTIVE_URL));
+
+	}
+
+	public function testAddSessionMultiple()
+	{
+
+		$dispatcher = static::getInstance();
+
+		$dispatcher->addSession(array(new Session('http://example.net/'), new Session('http://example.org/')));
+
+		$sessions = $dispatcher->getSessions();
+
+		$this->assertEquals('http://example.net/', $sessions[1]->getInfo(CURLINFO_EFFECTIVE_URL));
+		$this->assertEquals('http://example.org/', $sessions[2]->getInfo(CURLINFO_EFFECTIVE_URL));
+
+	}
+
+	public function testAddSessionException()
+	{
+
+		$this->setExpectedException('jyggen\\UnexpectedValueException');
+		static::getInstance()->addSession('session');
 
 	}
 
