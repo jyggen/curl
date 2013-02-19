76eb901b421eefec05f39ddb772fbdefe07d0897
Jonas Stendahl <jonas.stendahl@gmail.com>
2013-02-19 16:43:25 +0100

    Removed old unit tests.

--- tests/CurlTests.php
+++ tests/CurlTests.php
@@ -11,68 +11,9 @@
  */
 
 use jyggen\Curl;
-use Mockery as m;
 
 class CurlTests extends PHPUnit_Framework_TestCase
 {
-
-	protected $dispatcher;
-	protected $session;
-
-	public function setup()
-	{
-
-		$this->dispatcher = m::mock('jyggen\\Curl\\DispatcherInterface');
-		$this->session    = m::mock('jyggen\\Curl\\SessionInterface');
-
-		Curl::setDispatcher(get_class($this->dispatcher));
-		Curl::setSession(get_class($this->session));
-
-	}
-
-	public function teardown()
-	{
-
-		m::close();
-
-	}
-
-	public function testSetDispatcherAndGetDispatcher()
-	{
-
-		$this->assertEquals(get_class($this->dispatcher), Curl::getDispatcher());
-
-	}
-
-	/**
-     * @expectedException        jyggen\UnexpectedValueException
-     * @expectedExceptionMessage must implement
-     */
-	public function testSetDispatcherWithError()
-	{
-
-		Curl::setDispatcher('foobar');
-
-	}
-
-
-	public function testSetSessionAndGetSession()
-	{
-
-		$this->assertEquals(get_class($this->session), Curl::getSession());
-
-	}
-
-	/**
-     * @expectedException        jyggen\UnexpectedValueException
-     * @expectedExceptionMessage must implement
-     */
-	public function testSetSessionWithError()
-	{
-
-		Curl::setSession('foobar');
-
-	}
 
 	public function testDelete()
 	{
