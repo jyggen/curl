681a472546c72941ac260907b5e31b0437043a02
Jonas Stendahl <jonas.stendahl@gmail.com>
2013-02-22 15:17:45 +0100

    Namespaced exceptions and made them autoloadable.

---
+++ lib/jyggen/Curl/Exception/CurlErrorException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * A simple and lightweight cURL library with support for multiple requests in parallel.
+ *
+ * @package     Curl
+ * @version     2.0
+ * @author      Jonas Stendahl
+ * @license     MIT License
+ * @copyright   2013 Jonas Stendahl
+ * @link        http://github.com/jyggen/curl
+ */
+
+namespace jyggen\Curl\Exception;
+
+class CurlErrorException extends \Exception { }
